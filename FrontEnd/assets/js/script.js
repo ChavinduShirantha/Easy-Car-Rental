0b8665e0db61581710a9e8b34c69c30e155f6368
ChavinduShirantha <chavindushirantha@gmail.com>
2023-11-21 15:10:29 +0530

    Updated

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,3 +1,4 @@
+
 let contents = ["Rent a Car Directly From Owner ", "Rent a Car Anywhere Anytime ", "SRI LANKA’S NO. 1 VEHICLE SHARING NETWORK "];
 
 let charCount = 0;
@@ -31,3 +32,67 @@ function animateText() {
     }
 
 };
+
+$(document).ready(function() {
+    $("#homeBtn>a").css("color", "#00a26e");
+    $("#aboutBtn>a").css("color", "#ffffff");
+    $("#serviceBtn>a").css("color", "#ffffff");
+    $("#carsBtn>a").css("color", "#ffffff");
+    $("#clientBtn>a").css("color", "#ffffff");
+    $("#contactBtn>a").css("color", "#ffffff");
+});
+
+
+$("#homeBtn").click(function() {
+    $("#homeBtn>a").css("color", "#00a26e");
+    $("#aboutBtn>a").css("color", "#ffffff");
+    $("#serviceBtn>a").css("color", "#ffffff");
+    $("#carsBtn>a").css("color", "#ffffff");
+    $("#clientBtn>a").css("color", "#ffffff");
+    $("#contactBtn>a").css("color", "#ffffff");
+});
+
+$("#aboutBtn").click(function() {
+    $("#homeBtn>a").css("color", "#ffffff");
+    $("#aboutBtn>a").css("color", "#00a26e");
+    $("#serviceBtn>a").css("color", "#ffffff");
+    $("#carsBtn>a").css("color", "#ffffff");
+    $("#clientBtn>a").css("color", "#ffffff");
+    $("#contactBtn>a").css("color", "#ffffff");
+});
+
+$("#serviceBtn").click(function() {
+    $("#homeBtn>a").css("color", "#ffffff");
+    $("#aboutBtn>a").css("color", "#ffffff");
+    $("#serviceBtn>a").css("color", "#00a26e");
+    $("#carsBtn>a").css("color", "#ffffff");
+    $("#clientBtn>a").css("color", "#ffffff");
+    $("#contactBtn>a").css("color", "#ffffff");
+});
+
+$("#carsBtn").click(function() {
+    $("#homeBtn>a").css("color", "#ffffff");
+    $("#aboutBtn>a").css("color", "#ffffff");
+    $("#serviceBtn>a").css("color", "#ffffff");
+    $("#carsBtn>a").css("color", "#00a26e");
+    $("#clientBtn>a").css("color", "#ffffff");
+    $("#contactBtn>a").css("color", "#ffffff");
+});
+
+$("#clientBtn").click(function() {
+    $("#homeBtn>a").css("color", "#ffffff");
+    $("#aboutBtn>a").css("color", "#ffffff");
+    $("#serviceBtn>a").css("color", "#ffffff");
+    $("#carsBtn>a").css("color", "#ffffff");
+    $("#clientBtn>a").css("color", "#00a26e");
+    $("#contactBtn>a").css("color", "#ffffff");
+});
+
+$("#contactBtn").click(function() {
+    $("#homeBtn>a").css("color", "#ffffff");
+    $("#aboutBtn>a").css("color", "#ffffff");
+    $("#serviceBtn>a").css("color", "#ffffff");
+    $("#carsBtn>a").css("color", "#ffffff");
+    $("#clientBtn>a").css("color", "#ffffff");
+    $("#contactBtn>a").css("color", "#00a26e");
+});

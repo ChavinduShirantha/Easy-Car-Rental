8e0c33a4be2b81aadbd8eb195addbdcf132a8be9
ChavinduShirantha <chavindushirantha@gmail.com>
2023-12-02 08:33:08 +0530

    Updated Content

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -32,6 +32,18 @@ function animateText() {
 
 };
 
+function aos_init() {
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    });
+}
+window.addEventListener('load', () => {
+    aos_init();
+});
+
 $(document).ready(function () {
     $("#homeBtn>a").css("color", "#00a26e");
     $("#aboutBtn>a").css("color", "#ffffff");

bcda63632cbb5e51ba88ba86ed0a5c9f072c4713
ChavinduShirantha <chavindushirantha@gmail.com>
2023-12-05 11:45:51 +0530

    Added Functions for save Customer

---
+++ FrontEnd/assets/js/alert.js
@@ -0,0 +1,44 @@
+function saveUpdateAlert(vale, value2) {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: vale + ' has been ' + value2,
+        showConfirmButton: false,
+        timer: 2500
+    });
+}
+
+function unSuccessUpdateAlert(vale,vale2) {
+    Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: vale + " "+vale2,
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+
+function emptyMassage(value) {
+    let timerInterval
+    Swal.fire({
+        title:value +' Empty Result!',
+        html: 'I will close in <b></b> milliseconds.',
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+                b.textContent = Swal.getTimerLeft()
+            }, 100)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer')
+        }
+    })
+}
--- FrontEnd/controller/customerRegisterFormController.js
+++ FrontEnd/controller/customerRegisterFormController.js
@@ -1,3 +1,26 @@
+let baseURL = "http://localhost:8080/BackEnd_war/";
+
+$("#btnRegisterCustomer").click(function () {
+    let customerFormData = new FormData($("#registerForm")[0]);
+    $.ajax({
+        url: baseURL + "customer",
+        method: "post",
+        data: customerFormData,
+        contentType: false,
+        processData: false,
+        success: function (res) {
+            // alert(res.message)
+            saveUpdateAlert("Customer", res.message);
+        },
+        error: function (error) {
+            // alert(error.responseJSON.message);
+            unSuccessUpdateAlert("Customer", JSON.parse(error.responseText).message);
+        }
+    });
+});
+
+
+
 function loadSelectedImage(divId) {
 
     $(divId).on("change", function (e) {

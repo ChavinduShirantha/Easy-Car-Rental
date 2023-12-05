dcbc3ccbf38c73d34bd0424347356c12c0242b7a
ChavinduShirantha <chavindushirantha@gmail.com>
2023-12-05 15:52:29 +0530

    Added DriverService & DriverServiceImpl

---
+++ BackEnd/src/main/java/lk/easy_car_rental/service/DriverService.java
@@ -0,0 +1,12 @@
+package lk.easy_car_rental.service;
+
+import lk.easy_car_rental.dto.DriverDTO;
+
+/**
+ * @author : Chavindu
+ * created : 12/5/2023-3:51 PM
+ **/
+
+public interface DriverService {
+    void saveDriver(DriverDTO dto);
+}
---
+++ BackEnd/src/main/java/lk/easy_car_rental/service/impl/DriverServiceImpl.java
@@ -0,0 +1,16 @@
+package lk.easy_car_rental.service.impl;
+
+import lk.easy_car_rental.dto.DriverDTO;
+import lk.easy_car_rental.service.DriverService;
+
+/**
+ * @author : Chavindu
+ * created : 12/5/2023-3:51 PM
+ **/
+
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public void saveDriver(DriverDTO dto) {
+
+    }
+}

a1eaf6200503a8d915dae38f0c4e8774e67a481d
ChavinduShirantha <chavindushirantha@gmail.com>
2023-12-05 10:36:19 +0530

    Added WebRootConfig class

---
+++ BackEnd/src/main/java/lk/easy_car_rental/config/WebRootConfig.java
@@ -0,0 +1,15 @@
+package lk.easy_car_rental.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author : Chavindu
+ * created : 12/5/2023-10:35 AM
+ **/
+@Configuration
+@Import({JPAConfig.class})
+@ComponentScan(basePackages = "lk.easy_car_rental.service")
+public class WebRootConfig {
+}
